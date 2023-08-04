522fe3f546b9c16bb3bd392d7eedec8a06cd9545
Gabriel Baratella <gabrielbaratellaoliveira@gmail.com>
2023-08-03 22:29:50 -0300

    Correção pom.xml

---
+++ src/main/java/br/com/gabrielbaratella/bookstore/BookStore/Controller/BookController.java
@@ -0,0 +1,17 @@
+package br.com.gabrielbaratella.bookstore.BookStore.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class BookController {
+	@GetMapping("/")
+	public String home() {
+		return "home";
+	}
+	@GetMapping("/book_register")
+	public String bookRegister() {
+		return "bookRegister";
+	}
+	
+}
